9895bb6dfebcaa247d4479070ba841ba015bd330
Steve Renaker <renaker@yahoo.com>
2014-08-05 16:06:21 -0700

    cleaning up

--- anti_vowel.py
+++
@@ -1,9 +0,0 @@
-def anti_vowel(text):
-    vowels = ["A", 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u']
-    result = ''
-    for t in text:
-        if t not in vowels:
-            result += t
-    print result
-
-anti_vowel("Hey you")
--- reverse.py
+++
@@ -1,9 +0,0 @@
-def reverse(text):
-    i = len(text) - 1
-    result = ""
-    while i >= 0:
-        result += text[i]
-        i -= 1
-    print result
-
-reverse("Python!")
